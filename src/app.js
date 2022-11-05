fbd95f28b1d0d00c6c86e0a453322135afe50fc1
BrayanSO <briansolera@gmail.com>
2022-11-05 16:07:02 +0000

    primer

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+ 
+function generardominios () {
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+  let dominios = [];
+
+  for( let 1 = 0 ; i < pronoun.length; 1++) {
+    for (let j = 0; j < adj.length  1++) {
+      for (let k= 0 ; <nouns.length k++>) {
+      dominios.push(pronoun[i] + adj[j] + nouns[k] + ".com");
+    }
+      }
+    }
+ document.getElementById("dominios").innerHTML= dominios.join("<br>")
+
+}
