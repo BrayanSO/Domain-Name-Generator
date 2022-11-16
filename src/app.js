e586ff624a7da04be4278af757f62bb786a06687
BrayanSO <briansolera@gmail.com>
2022-11-16 20:28:45 +0000

    segundo arreglo

--- src/app.js
+++ src/app.js
@@ -8,21 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", generarDominios);
 };
- 
-function generardominios () {
-  let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
-  let dominios = [];
 
-  for( let 1 = 0 ; i < pronoun.length; 1++) {
-    for (let j = 0; j < adj.length  1++) {
-      for (let k= 0 ; <nouns.length k++>) {
-      dominios.push(pronoun[i] + adj[j] + nouns[k] + ".com");
-    }
+function generarDominios() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominios = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let dominios = pronoun[i] + adj[j] + noun[k] + ".com";
+        dominios =
+          dominios + '<li class= "list-group-item">' + dominios + "<li>";
       }
     }
- document.getElementById("dominios").innerHTML= dominios.join("<br>")
-
+  }
+  document.getElementById("generarDominios").innerHTML = dominios;
 }
